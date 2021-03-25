4230906774ecac75760617fec15a5e1023db3bd0
Tejassonar <sonartejas908@gmail.com>
2021-03-25 16:10:41 +0530

    Added:Case statement employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,15 +4,16 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
 
-if [ $empCheck -eq $IS_FULL_TIME ]
-then
-  workHrs=8
-elif [ $empCheck -eq $IS_PART_TIME ]
-then
-  workHrs=4
-else
-  workHrs=0
-fi
+case $empCheck in
+	$IS_FULL_TIME)
+		workHrs=8
+	;;
+	$IS_PART_TIME)
+		workHrs=4
+	;;
+	*)
+		workHrs=0
+	;;
+esac
 salary=$(($EMP_RATE_PER_HR*$workHrs))
-echo "Total salary is $salary"
-
+echo "Total Salary is $salary"
