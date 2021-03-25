07786117127c87aff204ad843a003c3938ba83b7
Tejassonar <sonartejas908@gmail.com>
2021-03-25 16:56:25 +0530

    Added:Refactored code using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,30 +3,33 @@
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
-MAX_HRS_IN_MONTH=100
+MAX_HRS_IN_MONTH=50
 NUM_WORKING_DAYS=20
 
 
 totalEmpHrs=0
 totalWorkingDays=0
 
+function getWorkingHrs() {
+	case $1 in
+      $IS_FULL_TIME)
+         workHrs=8
+      ;;
+      $IS_PART_TIME)
+         workHrs=4
+      ;;
+      *)
+         workHrs=0
+      ;;
+   esac
+	echo $workHrs
+}
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-case $empCheck in
-
-	$IS_FULL_TIME)
-		workHrs=8
-	;;
-	$IS_PART_TIME)
-		workHrs=4
-	;;
-	*)
-		workHrs=0
-	;;
-esac
-totalEmpHrs=$(($totalEmpHrs + $workHrs))
+	workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+	totalEmpHrs=$(($totalEmpHrs + $workHrs))
 done
 	totalSalary=$(($EMP_RATE_PER_HR*$totalEmpHrs))
-	echo  "Total salary is : $totalSalary"
+	echo "Total Salary is $totalSalary"
+
